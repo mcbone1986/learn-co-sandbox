2e1d1b2a9d4029a64ca10fd3238977dcdfa1e3af
Matthew Bonebrake <matthewcbonebrake@gmail.com>
2021-03-02 19:08:18 +0000

    Automatically backed up by Learn

---
+++ case.rb
@@ -0,0 +1,8 @@
+greeting = "unfriendly_greeting"
+case greeting
+  when "unfriendly_greeting"
+    puts "Whatcha wahnt?"
+  when "friendly_greeting"
+    puts "Hi! Howdy!"
+end
+    
---
+++ selection.rb
@@ -0,0 +1,9 @@
+#if TRUE
+#  puts "This code runs!"
+#end
+
+def greeting(name = "Neighbor")
+  puts "Hello, #{name}."
+end
+
+greeting("george")
